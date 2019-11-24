93133b9176a14efab174accf6089d5e9301fcbb9
ahmedElmaghr <elmaghraoui.ahmed@gmail.com>
2019-11-24 18:38:25 +0100

    Updates

---
+++ precache-manifest.cf7569958c65a9caa89bd0eaae826ed7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b1760996c31cc2106a90127ea9c24a2",
+    "url": "/mediaFrancais/index.html"
+  },
+  {
+    "revision": "9c81b27bea13f062478a",
+    "url": "/mediaFrancais/static/css/main.3b5d1a30.chunk.css"
+  },
+  {
+    "revision": "161b0a684e2342cba395",
+    "url": "/mediaFrancais/static/js/2.1461ef4c.chunk.js"
+  },
+  {
+    "revision": "9c81b27bea13f062478a",
+    "url": "/mediaFrancais/static/js/main.2ece3069.chunk.js"
+  },
+  {
+    "revision": "f51d50e344fb0f1db216",
+    "url": "/mediaFrancais/static/js/runtime-main.358bd96d.js"
+  },
+  {
+    "revision": "6491e171067cc514e979566eda72b805",
+    "url": "/mediaFrancais/static/media/medias_francais.6491e171.tsv"
+  },
+  {
+    "revision": "d49ff9ffc649a7eea05611350f4dc369",
+    "url": "/mediaFrancais/static/media/relations_medias_francais.d49ff9ff.tsv"
+  }
+]);
